a4fefa125b4d8f0da6730dad70b6b8d1a3347abd
Alexandre Lima <alexandre.cyber@outlook.com>
2020-01-07 17:47:47 -0300

    Update backup_elastic.py

--- backup_elastic.py
+++ backup_elastic.py
@@ -1,13 +1,13 @@
 import subprocess
 import requests
 
-request = requests.get("http://10.99.40.25:9200/_all")
+request = requests.get("http://exemplo.com:9200/_all")
 indexs = list(request.json())
 
 for i in indexs:
     subprocess.run(["docker", "run", "--rm", "-ti", "taskrabbit/elasticsearch-dump",
-                    "--input=http://10.99.40.25:9200/{}".format(i),
-                    "--output=http://10.99.40.28:9200/{}".format(i),
+                    "--input=http://exemplo.com:9200/{}".format(i),
+                    "--output=http://exemplo.com:9200/{}".format(i),
                     "--type=data"])
 
 print("############## BACKUP COMPLETE ###############")
